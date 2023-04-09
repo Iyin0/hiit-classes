f4be38d4e9ebe8ac33a4fb410a0c8a65f667721f
Iyin0 <p2iyinoluwa@gmail.com>
2023-04-09 15:07:58 +0100

    class-3

--- Class-2/myBlog/server/routes/accountRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const accounts = require('../controllers/accountController')
-
-
-router.post('/auth/signup', accounts.signUp)
-router.post('/auth/login', accounts.login)
-
-module.exports = router
--- Class-2/projectFour/data.js
+++
@@ -1,9 +0,0 @@
-const persons = ['Chris', 'Aba', 'Esther', 'Jethro']
-const age = [20, 24, 35, 26]
-
-console.log(persons)
-
-module.exports = {
-    persons,
-    age
-}
