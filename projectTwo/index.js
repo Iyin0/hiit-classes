c90c561f89ab33f780ff0c1fc1845f4a5d482687
iyin0 <p2iyinoluwa@gmail.com>
2023-08-16 12:02:38 +0100

    introdution to javascript

---
+++ projectTwo/index.js
@@ -0,0 +1,31 @@
+// console.log('Welcome to Class')
+
+// var fullName = 'Eneh Love'
+// console.log(fullName)
+// fullName = 'Khadija Bello'
+
+// console.log(fullName)
+
+// let fullName = 'Eneh Love'
+// console.log(fullName)
+// fullName = 'Khadija Bello'
+
+// console.log(fullName)
+
+// const fullName = 'Eneh Love'
+// console.log(fullName)
+// fullName = 'Khadija Bello'
+
+// console.log(fullName)
+
+let name = 'Emeka'
+let $name = 'Ebube'
+// let 5name = 'Love'
+
+let age = null
+let height = "5'8"
+
+height = 1.7
+
+console.log(age)
+console.log(height)
