dcd1761cb638bcb40883425c9a47ea229bae8796
iyin0 <p2iyinoluwa@gmail.com>
2023-10-12 15:14:31 +0100

    updating table

--- projectThree/index.js
+++ projectThree/index.js
@@ -1,40 +1,70 @@
-let allProducts = [
-    {
-        name: 'Fan',
-        description: 'For Blowing Air',
-        price: '400'
-    },
-    {
-        name: 'Chair',
-        description: 'For sitting',
-        price: '4000'
-    },
-    {
-        name: 'Table',
-        description: 'For palcing items',
-        price: '40'
-    },
-    {
-        name: 'Screen',
-        description: 'For viewing',
-        price: '400'
-    },
-    {
-        name: 'Board',
-        description: 'For writing',
-        price: '400'
-    },
-    {
-        name: 'Marker',
-        description: 'For writing on a board',
-        price: '7000'
-    },
-    {
-        name: 'Table',
-        description: 'For placing tools',
-        price: '7000'
-    },
-]
+import { allProducts } from "./allProducts.js"
+
+// FORM
+const form = document.createElement('form')
+
+const formBody = ['Name', 'Price', 'Description']
+
+formBody.forEach(element => {
+    const container = document.createElement('div')
+    form.appendChild(container)
+
+    const label = document.createElement('label')
+    label.innerText = element
+    container.appendChild(label)
+
+    if (element === 'Name') {
+        const input = document.createElement('input')
+        input.setAttribute('type', 'text')
+        input.setAttribute('class', 'name')
+        container.appendChild(input)
+    } else if (element === 'Price') {
+        const input = document.createElement('input')
+        input.setAttribute('type', 'number')
+        input.setAttribute('class', 'price')
+        container.appendChild(input)
+    } else {
+        const textarea = document.createElement('textarea')
+        textarea.setAttribute('class', 'description')
+        container.appendChild(textarea)
+    }
+})
+
+const formBtn = document.createElement('button')
+formBtn.setAttribute('type', 'submit')
+formBtn.innerText = 'Submit'
+form.appendChild(formBtn)
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const name = document.querySelector('.name').value
+    const price = document.querySelector('.price').value
+    const description = document.querySelector('.description').value
+
+    if (name && price && description) {
+        const item = {
+            name,
+            description,
+            price
+        }
+        // console.log(allProducts)
+        allProducts.push(item)
+        // console.log(allProducts)
+        removeTableBody()
+        appendTableBody()
+
+        document.querySelector('.name').value = ''
+        document.querySelector('.price').value = ''
+        document.querySelector('.description').value = ''
+
+    } else {
+        alert('All fields must be field')
+    }
+})
+
+document.body.appendChild(form)
 
 // TABLE
 
@@ -60,32 +90,54 @@ headers.forEach(element => {
     tableHeadRow.appendChild(th)
 })
 
+const removeTableBody = () => {
+    const tableBody = document.querySelector('tbody')
+    table.removeChild(tableBody)
+}
 
-// create table body
-const tableBody = document.createElement('tbody')
-table.appendChild(tableBody)
-
-allProducts.forEach((row, index) => {
-    const tableBodyRow = document.createElement('tr')
-    tableBody.appendChild(tableBodyRow)
-
-    const td1 = document.createElement('td')
-    td1.innerText = index + 1
-    tableBodyRow.appendChild(td1)
-
-    const td2 = document.createElement('td')
-    td2.innerText = row.name
-    tableBodyRow.appendChild(td2)
-
-    const td3 = document.createElement('td')
-    td3.innerText = row.description
-    tableBodyRow.appendChild(td3)
-
-    const td4 = document.createElement('td')
-    td4.innerText = row.price
-    tableBodyRow.appendChild(td4)
-})
+const removeTableItem = (index) => {
+    allProducts = allProducts.filter(item => allProducts.indexOf(item) !== index)
+    removeTableBody()
+    appendTableBody()
+    // console.log(allProducts)
+}
 
 
+const appendTableBody = () => {
+    // create table body
+    const tableBody = document.createElement('tbody')
+    table.appendChild(tableBody)
+
+    allProducts.forEach((row, index) => {
+        const tableBodyRow = document.createElement('tr')
+        tableBody.appendChild(tableBodyRow)
+
+        const td1 = document.createElement('td')
+        td1.innerText = index + 1
+        tableBodyRow.appendChild(td1)
+
+        const td2 = document.createElement('td')
+        td2.innerText = row.name
+        tableBodyRow.appendChild(td2)
+
+        const td3 = document.createElement('td')
+        td3.innerText = row.description
+        tableBodyRow.appendChild(td3)
+
+        const td4 = document.createElement('td')
+        td4.innerText = row.price
+        tableBodyRow.appendChild(td4)
+
+        const rmvBtn = document.createElement('button')
+        rmvBtn.innerText = 'Remove'
+
+        rmvBtn.addEventListener('click', () => {
+            removeTableItem(index)
+        })
+        tableBodyRow.appendChild(rmvBtn)
+    })
+}
+
+appendTableBody()
 
 document.body.appendChild(table)
