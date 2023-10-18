2bf112cc642899ff04273bf8d67b4e175de0d3b6
iyin0 <p2iyinoluwa@gmail.com>
2023-10-18 15:35:43 +0100

    intro to node

---
+++ node/intro.js
@@ -0,0 +1,14 @@
+console.log('Welcome to class')
+
+const greeting = (name) => {
+    console.log(`Welcome ${name}`)
+}
+
+greeting('James')
+
+const users = ['James', 'Nonye', 'Okiki', "Daniel", 'Theresa']
+
+const age = [29, 27, 30, 15, 12]
+
+console.log(users)
+console.log(age)
