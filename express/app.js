a5a39b76772425980b60fd3670e0dc1d20ff6b92
iyin0 <p2iyinoluwa@gmail.com>
2023-12-14 13:19:07 +0100

    intro to db

--- express/app.js
+++ express/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+require('dotenv').config()
 const cors = require('cors')
 const uuid = require('uuid')
 const bodyParser = require('body-parser')
+const db = require('./db')
 
 const app = express()
 
@@ -18,16 +20,29 @@ app.use(cors({
 }))
 
 
-app.get('/getComments', (req, res) => {
-    const data = {
-        id: '2hnefbf9iw03',
-        name: 'Daniel',
-        comment: 'I come late to class'
+app.get('/getComments', async (req, res) => {
+    try {
+        const sql = 'SELECT * FROM comments'
+        const [data] = await db.execute(sql)
+
+        let response = {
+            data,
+            statusCode: 200,
+            message: 'Comment posted successfull',
+        }
+
+        res.status(200).json(response)
+
+    } catch (error) {
+        const response = {
+            message: error.message,
+            statusCode: 400
+        }
+        res.status(400).json(response)
     }
-    res.status(200).json(data)
 })
 
-app.post('/postComment', (req, res) => {
+app.post('/postComment', async (req, res) => {
 
     const { name, comment } = req.body
     const id = uuid.v4()
@@ -36,18 +51,44 @@ app.post('/postComment', (req, res) => {
         if (!name) throw Error('Name is required')
         if (!comment) throw Error('Comment is required')
 
+        const sql = `INSERT INTO comments (id, name, comment) VALUES ("${id}", "${name}", "${comment}");`
+        await db.execute(sql)
 
         let response = {
-            data: {
-                id,
-                name,
-                comment,
-            },
             statusCode: 200,
             message: 'Comment posted successfull',
         }
 
         res.status(200).json(response)
+    } catch (error) {
+        const response = {
+            message: error.message,
+            statusCode: 400
+        }
+        res.status(400).json(response)
+    }
+})
+
+
+app.get('/getComment', async (req, res) => {
+
+    const { id } = req.query
+
+    console.log(id)
+    try {
+
+        if (!id) throw Error('Comment ID is required')
+        const sql = `SELECT * FROM comments WHERE id='${id}'`
+        const [data] = await db.execute(sql)
+
+        let response = {
+            data: data[0],
+            statusCode: 200,
+            message: 'Comment posted successfull',
+        }
+
+        res.status(200).json(response)
+
     } catch (error) {
         const response = {
             message: error.message,
