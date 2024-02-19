952ed761c2493a94482e56359bf9b8b0e9d1c437
iyin0 <p2iyinoluwa@gmail.com>
2024-02-19 11:24:03 +0100

    intro to functions

---
+++ projectTwo/functions.js
@@ -0,0 +1,52 @@
+// function logToConsole() {
+//     console.log("Class started")
+// }
+
+// logToConsole()
+
+let a = 5
+let b = 6
+
+const areaRectangle = (a, b) => {
+    let area = a * b
+    console.log(area)
+
+    return area
+}
+
+const perimeterRectangle = () => {
+    let peri = 2 * (a + b)
+    console.log(peri)
+}
+
+
+areaRectangle(7, 8)
+perimeterRectangle()
+
+let areaRect = areaRectangle(7, 8)
+
+console.log(areaRect)
+
+let scores = []
+
+for (let i = 0; i < 20; i++) {
+    scores[i] = Math.floor(Math.random() * 100)
+}
+
+console.log(scores)
+
+const printTotal = (scores) => {
+    let total = 0
+
+    for (let i = 0; i < scores.length; i++) {
+        if (scores[i] > 50) {
+            total = total + scores[i]
+        }
+
+        console.log(total)
+    }
+
+    return total
+}
+
+console.log(printTotal(scores))
