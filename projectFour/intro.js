1edd0f4a7fb360c0fcaf405fa5414e9068ae89e8
iyin0 <p2iyinoluwa@gmail.com>
2024-03-04 11:55:57 +0100

    intro to node.js

---
+++ projectFour/intro.js
@@ -0,0 +1,13 @@
+console.log("Welcome to Class")
+
+const greeting = (name) => {
+    console.log(`Good morning ${name}`)
+}
+
+greeting("Daniel")
+
+const users = ['Daniel', 'Emeka', 'David', 'Simon']
+const age = [29, 17, 15, 30, 40]
+
+console.log(users)
+console.log(age)
