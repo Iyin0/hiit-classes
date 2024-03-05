84f25e9a3742e5af7c2459ccbe6c60e589eca31d
iyin0 <p2iyinoluwa@gmail.com>
2024-03-05 11:04:09 +0100

    intro to modules

--- projectFour/intro.js
+++ projectFour/intro.js
@@ -1,13 +1,21 @@
-console.log("Welcome to Class")
+const os = require('os')
+const data = require('./data')
+
+// console.log("Welcome to Class")
 
 const greeting = (name) => {
     console.log(`Good morning ${name}`)
 }
 
-greeting("Daniel")
+// greeting("Daniel")
 
-const users = ['Daniel', 'Emeka', 'David', 'Simon']
-const age = [29, 17, 15, 30, 40]
+// console.log(os)
+// console.log(__dirname)
+// console.log(__filename)
+
+// console.log(data)
+
+const { users, age } = data
 
 console.log(users)
 console.log(age)
