548b770950237a627bc79d4d0427b9f99e1bbd86
iyin0 <p2iyinoluwa@gmail.com>
2024-03-13 13:38:08 +0100

    intro to dbms

--- express/app.js
+++ express/app.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const uuid = require('uuid');
+const db = require('./db');
 
 
 const PORT = process.env.PORT || 4000;
@@ -26,27 +28,81 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/getBlogs', (req, res) => {
+app.get('/getBlogs', async (req, res) => {
 
-    const data = [
-        {
-            title: "My first class",
-            body: "This is my first class",
-            author: "Emeka"
-        }
-    ];
+    try {
+        let sql = 'SELECT * FROM blogs';
 
-    const response = {
-        message: "Fetched all blogs sucessfully",
-        statusCode: 200,
-        status: "success",
-        data
-    };
+        const [data] = await db.execute(sql);   // execute sql statement
 
-    res.status(200).json(response);
+        const response = {
+            message: "Fetched all blogs sucessfully",
+            statusCode: 200,
+            status: "success",
+            data
+        };
+
+        res.status(200).json(response);
+    } catch (error) {
+        const response = {
+            message: error.message,
+            statusCode: 400,
+            status: "failed",
+        };
+        res.status(400).json(response);
+    }
 });
 
-app.post('/postBlog', (req, res) => {
+// app.get('/getBlogs/allBlogs', (req, res) => {
+//     const response = {
+//         message: 'All Blogs response',
+//         statusCode: 200,
+//         status: "success",
+//     };
+//     res.status(200).json(response);
+// });
+
+app.get('/getBlog', async (req, res) => {
+    const { id } = req.query;
+
+    try {
+        if (!id) {
+            throw Error("Blog ID is required");
+        }
+
+        let sql = `SELECT * FROM blogs WHERE id="${id}"`;
+
+        const [data] = await db.execute(sql);   // execute sql statement
+
+        if (data.length < 1) {
+            const response = {
+                message: "Resource not found",
+                statusCode: 404,
+                status: "failed",
+            };
+            res.status(404).json(response);
+        } else {
+            const response = {
+                message: "Fetched blog sucessfully",
+                statusCode: 200,
+                status: "success",
+                data
+            };
+
+            res.status(200).json(response);
+        }
+
+    } catch (error) {
+        const response = {
+            message: error.message,
+            statusCode: 400,
+            status: "failed",
+        };
+        res.status(400).json(response);
+    }
+});
+
+app.post('/postBlog', async (req, res) => {
 
     const { title, body, author } = req.body;
 
@@ -55,14 +111,116 @@ app.post('/postBlog', (req, res) => {
             throw Error("All fields are required");
         }
 
+        const id = uuid.v4();        // generate id
+
+        const sql = `INSERT INTO blogs(id, title, body, author) VALUES ("${id}", "${title}", "${body}", "${author}") `;
+        await db.execute(sql);
+
         const response = {
             message: "Successfully posted a blog",
             statusCode: 200,
             status: "success",
-            data: { title, body, author }
         };
 
         res.status(200).json(response);
+    } catch (error) {
+        const response = {
+            message: error.message,
+            statusCode: 400,
+            status: "failed",
+        };
+        res.status(400).json(response);
+    }
+});
+
+app.patch('/updateBlog/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const { title, body, author } = req.body;
+
+    try {
+        if (!id) {
+            throw Error("Blog ID is required");
+        }
+
+        if (!title && !body && !author) {
+            throw Error("Title, Body or Author is required");
+        }
+
+        let exitsSql = `SELECT * FROM blogs WHERE id = '${id}'`;
+
+        const [item] = await db.execute(exitsSql);
+
+        if (!item[0]) throw Error('Blog does not exist');
+
+        let updates = [];
+
+        if (title) {
+            updates.push(`title = '${title}'`);
+        }
+
+        if (body) {
+            updates.push(`body = '${body}'`);
+        }
+
+        if (author) {
+            updates.push(`author = '${author}'`);
+        }
+
+        if (updates.length === 0) {
+            throw Error("At least one of Title, Body, or Author must be provided");
+        }
+
+        const sql = `UPDATE blogs SET ${updates.join(', ')} WHERE id = '${id}'`;
+
+        // const sql = `UPDATE blogs SET title="${title}" WHERE id="${id}"`;
+
+        await db.execute(sql);
+
+        const response = {
+            message: "Blog updated successfully",
+            statusCode: 200,
+            status: "success",
+        };
+
+        res.status(200).json(response);
+
+    } catch (error) {
+        const response = {
+            message: error.message,
+            statusCode: 400,
+            status: "failed",
+        };
+        res.status(400).json(response);
+    }
+});
+
+app.delete('/deleteBlog/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        if (!id) {
+            throw Error("Blog ID is required");
+        }
+
+        let exitsSql = `SELECT * FROM blogs WHERE id = '${id}'`;
+
+        const [item] = await db.execute(exitsSql);
+
+        if (!item[0]) throw Error('Blog does not exist');
+
+        const sql = `DELETE FROM blogs WHERE id="${id}"`;
+        await db.execute(sql);
+
+        const response = {
+            message: "Blog deleted successfully",
+            statusCode: 200,
+            status: "success",
+        };
+
+        res.status(200).json(response);
+
+
     } catch (error) {
         const response = {
             message: error.message,
